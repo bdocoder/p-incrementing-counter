dbc3d3fb049e7bdcab08c9209f1ff575cc20151a
Abdullah Ahmad <bdocoder@protonmail.com>
2023-10-18 22:35:25 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import Counter from "./components/Counter.vue";
+
+const counters = [
+  {
+    text: "Twitter Followers",
+    target: 12000,
+    icon: "/twitter.svg",
+    alt: "twitter icon",
+  },
+  {
+    text: "Facebook Followers",
+    target: 75000,
+    icon: "/facebook.svg",
+    alt: "facebook icon",
+  },
+  {
+    text: "Youtube Subscribers",
+    target: 5000,
+    icon: "/youtube.svg",
+    alt: "youtube icon",
+  },
+];
+
+const progress = ref(0);
+
+onMounted(() => {
+  const timer = setInterval(() => {
+    if (progress.value >= 1) return clearInterval(timer);
+
+    progress.value += 0.005;
+  }, 5);
+});
+</script>
+
+<template>
+  <main class="hero is-fullheight is-light">
+    <div class="hero-body">
+      <div class="level" style="margin: 0 auto">
+        <Counter
+          v-for="counter in counters"
+          :text="counter.text"
+          :target="counter.target"
+          :icon="counter.icon"
+          :alt="counter.alt"
+          :current="progress"
+          class="level-item"
+          style="padding: 1rem 2rem"
+        />
+      </div>
+    </div>
+  </main>
+</template>
---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  target: number;
+  current: number;
+  text: string;
+  icon: string;
+  alt: string;
+}>();
+</script>
+
+<template>
+  <div style="text-align: center; flex-direction: column">
+    <img
+      class="image is-48x48"
+      :src="icon"
+      :alt="alt"
+      style="margin-bottom: 0.65rem"
+    />
+    <p class="heading">{{ text }}</p>
+    <p class="title">{{ Math.floor(target * current) }}</p>
+  </div>
+</template>
